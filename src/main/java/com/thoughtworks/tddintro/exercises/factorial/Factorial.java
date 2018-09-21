ea19895311f57fe5715563a59cb0ecaafa06b2bc
KyleJGreen <greenk4@nku.edu>
2018-09-21 14:31:40 -0500

    Fixed factorial so that all tests passed

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,17 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        int result = 1;
+
+        if(i < 0)
+            throw new IllegalArgumentException("n must be positive");
+
+        while(i > 1)
+        {
+            result *= i;
+            i--;
+        }
+
+        return result;
     }
 }
