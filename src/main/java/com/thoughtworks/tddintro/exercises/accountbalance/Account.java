d10dda08a26daee39329ef75c1f048054239cd71
KyleJGreen <greenk4@nku.edu>
2018-09-21 15:02:22 -0500

    Finished AccountTests

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,24 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account(int initBalance) {
+        balance = initBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int deposit(int amount) {
+        this.balance += amount;
+        return this.balance;
+    }
+
+    public int withdraw(int amount) {
+        if (amount <= this.balance)
+            this.balance -= amount;
+        return this.balance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,24 +1,26 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        Account account = new Account(100);
+        assertTrue(account.getBalance() < account.deposit(50));
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+        Account account = new Account(100);
+        assertTrue(account.getBalance() > account.withdraw(50));
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-
+        Account account = new Account(50);
+        assertThat(account.getBalance(), is(account.withdraw(100)));
     }
 }
